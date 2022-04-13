9e4332f08989e195363403a961adcb5c6b0084fe
Wolf <d.wolftripp@gmail.com>
2022-04-13 12:28:33 -0700

    fix app.config error

--- myapp/__init__.py
+++ myapp/__init__.py
@@ -18,8 +18,8 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 # set up connection to db
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/unit4"
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost:5432/unit4"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
